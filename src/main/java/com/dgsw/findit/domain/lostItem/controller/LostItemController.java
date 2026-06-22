8adec2521906a881c9b321a64d6a8bfd60609210
hyxx-su <legendary9899@dgsw.hs.kr>
2026-06-22 16:05:40 +0900

    feat: controller

--- src/main/java/com/dgsw/findit/domain/lostItem/controller/LostItemController.java
+++ src/main/java/com/dgsw/findit/domain/lostItem/controller/LostItemController.java
@@ -1,7 +1,23 @@
 package com.dgsw.findit.domain.lostItem.controller;
 
+import com.dgsw.findit.domain.lostItem.dto.LostItemCreateRequest;
+import com.dgsw.findit.domain.lostItem.entity.LostItemEntity;
+import com.dgsw.findit.domain.lostItem.service.LostItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LostItemController {
+    private final LostItemService lostItemService;
+
+    @PostMapping("/items")
+    public ResponseEntity<LostItemEntity> createLostItem(@RequestBody LostItemCreateRequest request) {
+        LostItemEntity lostItem = lostItemService.createLostItem(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(lostItem);
+    }
 }
