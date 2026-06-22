223c4ce11f4b140471180bae67e7b68726aebc69
hyxx-su <legendary9899@dgsw.hs.kr>
2026-06-22 16:05:50 +0900

    feat: Service

--- src/main/java/com/dgsw/findit/domain/lostItem/service/LostItemService.java
+++ src/main/java/com/dgsw/findit/domain/lostItem/service/LostItemService.java
@@ -1,7 +1,23 @@
 package com.dgsw.findit.domain.lostItem.service;
 
+import com.dgsw.findit.domain.lostItem.dto.LostItemCreateRequest;
+import com.dgsw.findit.domain.lostItem.entity.LostItemEntity;
+import com.dgsw.findit.domain.lostItem.repository.LostItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LostItemService {
+    private final LostItemRepository lostItemRepository;
+
+    public LostItemEntity createLostItem(LostItemCreateRequest request) {
+        LostItemEntity lostItem = new LostItemEntity();
+        lostItem.setItemName(request.getItemName());
+        lostItem.setFoundLocation(request.getFoundLocation());
+        lostItem.setDescription(request.getDescription());
+        lostItem.setStatus("보관중");
+
+        return lostItemRepository.save(lostItem);
+    }
 }
