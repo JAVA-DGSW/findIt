1607a46afa097d0ad3becf6fe9656593a8d27364
hyxx-su <legendary9899@dgsw.hs.kr>
2026-06-22 15:40:02 +0900

    feat: entity 구현

--- src/main/java/com/dgsw/findit/domain/lostItem/entity/LostItemEntity.java
+++ src/main/java/com/dgsw/findit/domain/lostItem/entity/LostItemEntity.java
@@ -1,13 +1,19 @@
 package com.dgsw.findit.domain.lostItem.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@Table(name="lost_item")
 public class LostItemEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
+    private Long id; // 물건 고유 아이디
+    private String itemName; // 물건명
+    private String foundLocation; // 발견 장소
+    private String description; // 설명
+    private String status; // 물건 상태
 }
