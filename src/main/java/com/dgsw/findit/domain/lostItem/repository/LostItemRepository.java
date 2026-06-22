a5768479128e8a83d5e25e9b69d9e3952c91463c
redjungi <redjungi09@gmail.com>
2026-06-22 15:20:42 +0900

    feat: Repositort상속 추가

--- src/main/java/com/dgsw/findit/domain/lostItem/repository/LostItemRepository.java
+++ src/main/java/com/dgsw/findit/domain/lostItem/repository/LostItemRepository.java
@@ -1,7 +1,9 @@
 package com.dgsw.findit.domain.lostItem.repository;
 
+import com.dgsw.findit.domain.lostItem.entity.LostItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LostItemRepository {
+public interface LostItemRepository extends JpaRepository<LostItemEntity, Long> {
 }
