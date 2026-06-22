582ec7003f8d08b8a7ff0db771ac6d8b332a7a7c
한승환 <coderrornotfound@gmail.com>
2026-06-22 16:00:41 +0900

    feat: 분실물 상세 조회 API 구현

--- src/main/java/com/dgsw/findit/domain/lostItem/controller/LostItemController.java
+++ src/main/java/com/dgsw/findit/domain/lostItem/controller/LostItemController.java
@@ -1,7 +1,21 @@
 package com.dgsw.findit.domain.lostItem.controller;
 
+import com.dgsw.findit.domain.lostItem.entity.LostItemEntity;
+import com.dgsw.findit.domain.lostItem.service.LostItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/items")
+@RequiredArgsConstructor
 public class LostItemController {
+    private final LostItemService service;
+
+    @GetMapping("/{id}")
+    public LostItemEntity getLostItem(@PathVariable Long id) {
+        return service.getLostItem(id);
+    }
 }
--- src/main/java/com/dgsw/findit/domain/lostItem/service/LostItemService.java
+++ src/main/java/com/dgsw/findit/domain/lostItem/service/LostItemService.java
@@ -1,7 +1,17 @@
 package com.dgsw.findit.domain.lostItem.service;
 
+import com.dgsw.findit.domain.lostItem.entity.LostItemEntity;
+import com.dgsw.findit.domain.lostItem.repository.LostItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LostItemService {
+    private final LostItemRepository repository;
+
+    public LostItemEntity getLostItem(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("분실물을 찾을 수 없습니다."));
+    }
 }
