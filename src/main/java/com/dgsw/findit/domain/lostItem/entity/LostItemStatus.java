7bc874d5ff8121e1f641e2aa38c2937ab0dcb3ca
redjungi <redjungi09@gmail.com>
2026-06-23 11:20:26 +0900

    feat: enum에 displayName추가

--- src/main/java/com/dgsw/findit/domain/lostItem/entity/LostItemStatus.java
+++ src/main/java/com/dgsw/findit/domain/lostItem/entity/LostItemStatus.java
@@ -1,6 +1,14 @@
 package com.dgsw.findit.domain.lostItem.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum LostItemStatus {
-    STORED, // 보관중
-    RETURNED // 반환완료
+
+    STORED("보관중"),
+    RETURNED("반환완료");
+
+    private final String displayName;
 }
