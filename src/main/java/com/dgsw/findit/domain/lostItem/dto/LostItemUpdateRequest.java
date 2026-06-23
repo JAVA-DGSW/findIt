fd45634cf7670bc01e397624931625eb886c95ba
hyxx-su <legendary9899@dgsw.hs.kr>
2026-06-23 11:28:11 +0900

    feat: UpdateRequest

--- src/main/java/com/dgsw/findit/domain/lostItem/dto/LostItemUpdateRequest.java
+++ src/main/java/com/dgsw/findit/domain/lostItem/dto/LostItemUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.dgsw.findit.domain.lostItem.dto;
 
+import com.dgsw.findit.domain.lostItem.entity.LostItemStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +10,5 @@ public class LostItemUpdateRequest {
     private String itemName;
     private String foundLocation;
     private String description;
-    private String status;
+    private LostItemStatus status;
 }
